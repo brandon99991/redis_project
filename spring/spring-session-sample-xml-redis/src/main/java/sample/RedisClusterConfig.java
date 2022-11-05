a04f4d6fcbc96fb040eed30f0c453ceafaeced99
DESK01\desk01 <induomo@gmail.com>
2022-11-05 20:52:03 +0900

    1

--- spring/spring-session-sample-xml-redis/src/main/java/sample/RedisClusterConfig.java
+++ spring/spring-session-sample-xml-redis/src/main/java/sample/RedisClusterConfig.java
@@ -19,38 +19,15 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import io.lettuce.core.ClientOptions;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
-//@Profile("embedded-redis")
 public class RedisClusterConfig {
 	
-	/*
-    @Autowired
-    RedisNodes redisNodes;
-    
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
-        redisNodes.getNodes().forEach(s -> {
-            String[] url = s.split(":");
-            redisClusterConfiguration.clusterNode(url[0],Integer.parseInt(url[1]));
-        });
-        return new LettuceConnectionFactory(redisClusterConfiguration);
-    }    
-    */
-	
-    private String host1= "192.168.35.211";
-    private int port1 = 6101;		
-    private String host2= "192.168.35.212";
-    private int port2 = 6101;		
-    private String host3= "192.168.35.213";
-    private int port3 = 6101;		
-    private String host4= "192.168.35.211";
-    private int port4 = 6201;		
-    private String host5= "192.168.35.212";
-    private int port5 = 6201;		
-    private String host6= "192.168.35.213";
-    private int port6 = 6201;		
+    //@Value("${redis.cluster}")
+    @Value("#{'${redis.cluster}'.split(',')}")
+    private List<String> redisNodes;
     
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
@@ -61,13 +38,12 @@ public class RedisClusterConfig {
               .build();
       
       RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
-      //clusterConfiguration.clusterNode(host, port);
-      clusterConfiguration.clusterNode(host1, port1);
-      clusterConfiguration.clusterNode(host2, port2);
-      clusterConfiguration.clusterNode(host3, port3);
-      clusterConfiguration.clusterNode(host4, port4);
-      clusterConfiguration.clusterNode(host5, port5);
-      clusterConfiguration.clusterNode(host6, port6);
+      
+      redisNodes.forEach(s -> {
+          String[] url = s.split(":");
+          clusterConfiguration.clusterNode(url[0],Integer.parseInt(url[1]));
+          System.out.println(url[0] + ":" +url[1]);
+      });
       
       new LettuceConnectionFactory(clusterConfiguration);
       return new LettuceConnectionFactory(clusterConfiguration, lettuceClientConfiguration);
